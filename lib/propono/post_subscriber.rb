9cc56c733d3bf67e4018c78fbc2aa6b0289352cf
Jeremy Walker <jez.walker@gmail.com>
2013-10-15 22:17:21 +0100

    Add post subscription

--- lib/propono/post_subscriber.rb
+++ lib/propono/post_subscriber.rb
@@ -1,12 +1,19 @@
 module Propono
   class PostSubscriber
+    include Sns
 
     def self.subscribe(topic, endpoint)
-      new.subscribe(topic, endpoint)
+      new(topic, endpoint).subscribe
     end
 
-    def subscribe(topic, endpoint)
+    def initialize(topic_id, endpoint)
+      @topic_id = topic_id
+      @endpoint = endpoint
     end
 
+    def subscribe
+      topic_arn = TopicCreator.find_or_create(@topic_id)
+      sns.subscribe(topic_arn, @endpoint, 'http')
+    end
   end
 end
--- test/post_subscriber_test.rb
+++ test/post_subscriber_test.rb
@@ -2,6 +2,34 @@ require File.expand_path('../test_helper', __FILE__)
 
 module Propono
   class PostSubscriberTest < Minitest::Test
+    def test_create_topic
+      topic = 'foobar'
+      TopicCreator.expects(:find_or_create).with(topic)
+      PostSubscriber.subscribe(topic, "foobar")
+    end
 
+    def test_everything
+      skip "I don't know how to do this"
+      topic = "Foobar"
+      PostSubscriber.subscribe(topic)
+    end
+
+    def test_subscribe_calls_subscribe
+      arn = "arn123"
+      endpoint = "http://meducation.net/some_queue_name"
+
+      TopicCreator.stubs(find_or_create: arn)
+
+      sns = mock()
+      sns.expects(:subscribe).with(arn, endpoint, 'http')
+      subscriber = PostSubscriber.new("Some topic", endpoint)
+      subscriber.stubs(sns: sns)
+      subscriber.subscribe
+    end
+
+    def test_it_correctly_uses_http_and_https
+      skip
+    end
   end
 end
+
