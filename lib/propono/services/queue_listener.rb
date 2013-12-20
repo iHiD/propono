4f3aae4d545a61cd5517b99dfb5560eed68ff755
Charles Care <c.p.care@gmail.com>
2013-12-20 10:53:15 +0000

    Fix unit tests

--- lib/propono/services/queue_listener.rb
+++ lib/propono/services/queue_listener.rb
@@ -78,7 +78,7 @@ module Propono
     end
 
     def move_to_failed_queue(sqs_message, exception)
-      next_queue = (sqs_message.failure_count < 3) ? queue_url : failed_queue_url
+      next_queue = (sqs_message.failure_count < 0) ? queue_url : failed_queue_url
       Propono.config.logger.error "Error proessing message, moving to queue: #{next_queue}"
       sqs.send_message(next_queue, sqs_message.to_json_with_exception(exception))
     end
