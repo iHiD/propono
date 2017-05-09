bed3a4b220224c6683350c1442f1747dbf0a070b
Michael Fich <michael@michaelfich.com>
2017-05-09 16:48:50 -0400

    update config to initialize access & service key
    refactor some code to conform to rubocop warnings and notifications

--- lib/propono/configuration.rb
+++ lib/propono/configuration.rb
@@ -1,23 +1,20 @@
 module Propono
-
   class ProponoConfigurationError < ProponoError
   end
 
   class Configuration
-
-    SETTINGS = [
-      :use_iam_profile, :access_key, :secret_key, :queue_region, :queue_suffix,
-      :application_name,
-      :udp_host, :udp_port,
-      :tcp_host, :tcp_port,
-      :logger,
-      :max_retries, :num_messages_per_poll
-    ]
-    attr_writer *SETTINGS
+    SETTINGS = %i[
+      use_iam_profile access_key secret_key queue_region queue_suffix
+      application_name udp_host udp_port tcp_host tcp_port logger
+      max_retries num_messages_per_poll
+    ].freeze
+    attr_writer(*SETTINGS)
 
     def initialize
       self.logger = Propono::Logger.new
-      self.queue_suffix = ""
+      self.queue_suffix = ''
+      self.access_key = ''
+      self.secret_key = ''
       self.use_iam_profile = false
       self.max_retries = 0
       self.num_messages_per_poll = 10
@@ -34,9 +31,9 @@ module Propono
     private
 
     def get_or_raise(setting)
-      val = instance_variable_get("@#{setting.to_s}")
-      val.nil?? raise(ProponoConfigurationError.new("Configuration for #{setting} is not set")) : val
+      val = instance_variable_get("@#{setting}")
+      raise ProponoConfigurationError, "Configuration for #{setting} is not set" if val.nil?
+      val
     end
   end
 end
-
