eaac206b151453b1580a185a4ac6b42bca7a313e
Douglas F Shearer <dougal.s@gmail.com>
2017-10-11 18:06:44 +0100

    Add test for missing logger.

--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -98,6 +98,13 @@ module Propono
       end
     end
 
+    def test_missing_logger_throws_exception
+      propono_config.logger = nil
+      assert_raises(ProponoConfigurationError) do
+        propono_config.logger
+      end
+    end
+
     def test_default_max_retries
       assert_equal 0, propono_config.max_retries
     end
